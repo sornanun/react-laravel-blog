89d43ac27b43c2c22bdb8fcf57943174efaa149c
sornanun <sornanun@gmail.com>
2019-10-15 16:20:02 +0700

    update

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -4,53 +4,48 @@ import ReactDOM from 'react-dom';
 /* Main Component */
 class Main extends Component {
 
-    constructor() {
+	constructor() {
 
-        super();
+		super();
 
-        /* currentProduct keeps track of the product currently
-         * displayed */
-        this.state = {
-            products: [],
-            currentProduct: null
-        }
-    }
+		/* currentProduct keeps track of the product currently
+		 * displayed */
+		this.state = {
+			products: [],
+			currentProduct: null
+		}
+	}
 
-    componentDidMount() {
-        //code omitted for brevity
-    }
+	componentDidMount() {
+		//code omitted for brevity
+	}
 
-    renderProducts() {
-        return this.state.products.map(product => {
-            return (
-                //this.handleClick() method is invoked onClick.
-                <li onClick={
-                () =>this.handleClick(product)} key={product.id} >
+	renderProducts() {
+		return this.state.products.map(product => {
+			return (
+				//this.handleClick() method is invoked onClick.
+				<li onClick={() =>this.handleClick(product)} key={product.id} >
                 { product.title } 
             </li>
-            );
-        })
-    }
+			);
+		})
+	}
 
-    handleClick(product) {
-        //handleClick is used to set the state
-        this.setState({ currentProduct: product });
+	handleClick(product) {
+		//handleClick is used to set the state
+		this.setState({ currentProduct: product });
 
-    }
+	}
 
-    render() {
-        return (
-            /* The extra divs are for the css styles */
-            <div>
-            <div>
-             <h3> All products </h3>
+	render() {
+		/* Some css code has been removed for brevity */
+		return (
+			<div>
               <ul>
                 { this.renderProducts() }
               </ul> 
-            </div> 
-           
-            <Product product={this.state.currentProduct} />
-        </div>
-        );
-    }
+            </div>
+
+		);
+	}
 }
